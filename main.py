2ced991bf0582fbf9f26f8c36d446c7caeb00025
Angelica <angelica.bohl@gmail.com>
2023-07-18 13:25:04 -0400

    First commit of main file with encode function

---
+++ main.py
@@ -0,0 +1,48 @@
+# Lab 6: Version Control - Group 2
+# UF COP3502C Summer 2023
+# Encoder: Angelica L Bohl
+# Decoder:
+
+
+def menu():
+    selection = None
+    in_menu = True
+    while in_menu:
+        print('Menu\n'
+              '-------------\n'
+              '1. Encode\n'
+              '2. Decode\n'
+              '3. Quit\n')
+        try:
+            selection = int(input('Please enter an option: '))
+        except:
+            print('Invalid selection!\n')
+            continue
+        in_menu = False
+    return selection
+
+
+def encode(pw):
+    encoded_pw = ''
+    for i in pw:
+        encoded_pw += str(int(i) + 3)
+    return encoded_pw
+
+
+encoding = True
+password = ''
+encoded_pw = ''
+
+while encoding:
+    selection = menu()
+    if selection == 1:
+        password = input('Please enter your password to encode: ')
+        encoded_pw = encode(password)
+        print('Your password has been encoded and stored!\n')
+    elif selection == 2:
+        pass
+    elif selection == 3:
+        break
+    else:
+        print('Invalid selection!\n')
+        continue
